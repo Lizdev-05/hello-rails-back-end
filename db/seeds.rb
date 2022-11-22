d535cd277d887be20f3661e11bd3b1d7f7f9829b
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-11-22 13:01:08 +0100

    Add migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greet1 = Greeting.create('How are you?')
+greet2 = Greeting.create('Hy?')
+greet3 = Greeting.create("What's up")
+greet4 = Greeting.create('How you dey na?')
+greet5 = Greeting.create('Hello')
